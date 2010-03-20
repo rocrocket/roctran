c29c1495bca9f25a398da8efe8231caabfd630f5
rocrocket <wupengchong@gmail.com>
2010-03-20 23:01:32 +0800

    roctran can send small file.
    [20100320]
    [  roc   ]
    [ change ]
    1.send small file.
    2.rocsend can set options and parameters.
    [  plan  ]
    1.change the transfer way to support sending large file.
    2.optimize the code,including add conf file.

--- src/rocreceive.c
+++ src/rocreceive.c
@@ -7,6 +7,134 @@
 #include<sys/types.h>
 #include<errno.h>
 
+#define DEBUG
+
+/*function:receiver one file from client
+ * param1:file descriptor
+ * param2:the local storing path
+*/
+int receive_file(int file_des,char *local_path)
+{
+/*read file name from client*/
+char file_name[1023],*str_p;
+str_p=file_name;
+ssize_t read_ret;
+read_ret=read(file_des,str_p,sizeof(file_name));
+if(read_ret==-1)
+{
+	perror("read FATAL");
+	return(1);
+}else{
+	#ifdef DEBUG
+	printf("filename:%s\n",str_p);
+	#endif
+}
+/*==========================*/
+
+/*concatenate two part into one file path*/
+printf("string operation start.\n");
+char file_path[200];
+char *path_p;
+file_path[0]='\0';
+path_p=file_path;
+#ifdef DEBUG
+printf("path_p1:%s\n",path_p);
+#endif
+
+path_p=strncat(path_p,local_path,49);
+#ifdef DEBUG
+printf("path_p2:%s\n",path_p);
+#endif
+
+path_p=strncat(path_p,"/",10);
+#ifdef DEBUG
+printf("path_p3:%s\n",path_p);
+#endif
+
+path_p=strncat(path_p,str_p,50);
+#ifdef DEBUG
+printf("path_p4:%s\n",path_p);
+#endif
+/*=======================================*/
+
+/*open local new file*/
+FILE *fp;
+fp=fopen(path_p,"wb");
+if(fp==NULL)
+{
+	perror("fopen FATAL");
+	return(2);
+}else{
+	#ifdef DEBUG
+	printf("fopen success!\n");
+	#endif
+}
+/*===================*/
+
+/*read file size from client*/
+long long int file_size;
+read_ret=read(file_des,&file_size,sizeof(file_size));
+if(read_ret==-1)
+{
+	perror("read FATAL");
+	return(3);
+}else{
+	#ifdef DEBUG
+	printf("file size:%lld\n",file_size);
+	#endif
+}
+/*==========================*/
+
+/*read file content from client*/
+char *file_buf;
+file_buf=(char *)malloc(file_size);
+if(file_buf==NULL)
+{
+	perror("malloc FATAL");
+	return(4);
+}else{
+	#ifdef DEBUG
+	printf("malloc success!\n");
+	#endif
+}
+read_ret=read(file_des,file_buf,file_size);
+if(read_ret==-1)
+{
+	perror("read FATAL");
+	return(5);
+}else{
+	#ifdef DEBUG
+	//printf("file content:%s\n",file_buf);
+	#endif
+}
+/*=============================*/
+
+/*write to local file*/
+size_t fwrite_ret;
+fwrite_ret=fwrite(file_buf,file_size,1,fp);
+if(fwrite_ret==0)
+{
+	perror("fwrite FATAL");
+	return(6);
+}
+/*===================*/
+
+/*close local file*/
+int fclose_ret;
+fclose_ret=fclose(fp);
+if(fclose_ret!=0)
+{
+	perror("flcose FATAL");
+	return(7);
+}else{
+	#ifdef DEBUG
+	printf("fclose success!\n");
+	#endif
+}
+/*================*/
+return(0);
+}
+
 int main(int argc,char *argv[])
 {
 int socket_fd;
@@ -26,6 +154,7 @@ struct in_addr inp;
 int inet_ret;
 //clear the structure
 memset(&server_addr,0,sizeof(struct sockaddr_in));
+//set socket address
 server_addr.sin_family=AF_INET;
 server_addr.sin_port=htons(21212);
 server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
@@ -65,23 +194,20 @@ if(newsocket_fd==-1)
 printf("One child call me!\n");
 /*================*/
 
-/*read from a client*/
-ssize_t read_ret;
-int receive_len;
-char receive_string[100];
-char *str_p=receive_string;
-
-//read_ret=read(newsocket_fd,&receive_len,sizeof(reiceive_len));	
-read_ret=read(newsocket_fd,&receive_string,99);	
-if(read_ret==-1)
+/*receive file*/
+int receive_ret;
+receive_ret=receive_file(newsocket_fd,".");
+if(receive_ret!=0)
 {
-	perror("read FATAL");
-	exit(4);
-}else{
-	printf("I receive!\n");
-	printf("RECV:%s\n",receive_string);
+	printf("receive_file failed.ret=%d\n",receive_ret);
+	exit(5);
 }
-/*================*/
+/*============*/
 
+/*close the fd*/
+close(newsocket_fd);
+close(socket_fd);
+/*============*/
+printf("good bye!\n");
 return(0);
 }
--- src/rocsend.c
+++ src/rocsend.c
@@ -3,13 +3,63 @@
 #include<stdlib.h>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<sys/stat.h>
 #include<errno.h>
 #include<netinet/in.h>
 #include<string.h>
+#include<libgen.h>
+
+#define MAX_NUM_OPTIONS 48
 #define DEBUG
+
+/*global variable*/
+//the file path which you want to send
+char file_path[1023];
+/*===============*/
+
+/*cope with options and parameters*/
+int parseOptions(int argc,char *argv[])
+{
+int _argc;
+char *_argv[MAX_NUM_OPTIONS];
+int opt;//opt:return value of function "pareseOptions"
+
+if((argc==2)&&(argv[1][0]!='-')){
+        //read the specified file
+        printf("Wrong command format.\n");
+	return(1);
+}else{
+	int i;
+	_argc=(argc>=MAX_NUM_OPTIONS)?(MAX_NUM_OPTIONS-1):argc;
+	for(i=0;i<_argc;++i){
+		_argv[i]=strdup(argv[i]);
+	}
+}
+optarg=NULL;
+while((opt=getopt(_argc,_argv,"f:h"))!=EOF){
+	switch(opt){
+		case 'f':
+			printf("optarg:%s\n",optarg);
+			strcpy(file_path,optarg);
+			break;
+		default:
+			printf("Help information");
+	}
+}
+return(0);
+}
+/*================================*/
 
 int main(int argc,char *argv[])
 {
+/*cope with options and parameters*/
+int parse_ret;
+parse_ret=parseOptions(argc,argv);
+if(parse_ret!=0){
+	printf("parseOptions return %d\n",parse_ret);
+	exit(7);
+}
+/*================================*/
 
 /*socket initial file descriptor for client*/
 int socket_fd;
@@ -22,7 +72,7 @@ if(socket_fd==-1)
 	exit(1);
 }
 #ifdef DEBUG
-	printf("socket init success\n");
+printf("socket init success\n");
 #endif
 /*=================*/
 
@@ -45,8 +95,10 @@ client_addr.sin_port=htons(21212);
 /*inet_aton function's return value is networking sequence,
  * so we do not need to htonl it.*/
 client_addr.sin_addr.s_addr=inp.s_addr;
+#ifdef DEBUG
 printf("%d\n",inp.s_addr);
 printf("%d\n",client_addr.sin_addr.s_addr);
+#endif
 /*==========*/
 
 /*connect operation*/
@@ -60,21 +112,88 @@ if(conn_res<0)
 }
 /*=================*/
 
-/*send string to server*/
+/*send file name to server*/
+printf("file_path:%s\n",file_path);
+
+char send_file_name[1023],*file_name_p;
 ssize_t write_ret;
-char send_string[]="roctran V0.0.1";
-write_ret=write(socket_fd,send_string,sizeof(send_string));
+strcpy(send_file_name,file_path);
+file_name_p=basename(send_file_name);
+printf("send_file_name:%s\n",file_name_p);
+write_ret=write(socket_fd,file_name_p,sizeof(send_file_name));
 if(write_ret==-1)
 {
 	perror("write FATAL");
-	exit(3);
-}else if(write_ret==0){
-	printf("nothing send\n");
-}else{
-	printf("send success!\n");
+	exit(7);
 }
-/*===================*/
+/*========================*/
 
+/*get the file size*/
+struct stat file_stat;
+int stat_ret;
+long long int file_size;
+stat_ret=stat(file_path,&file_stat);
+if(stat_ret==-1)
+{
+	perror("stat FATAL");
+	exit(3);
+}else{
+	file_size=file_stat.st_size;
+	printf("file size:%lld\n",file_size);	
+}
+/*=================*/
+
+/*send the file size to server*/
+write_ret=write(socket_fd,&file_size,sizeof(file_size));
+if(write_ret==-1)
+{
+	perror("write FATAL");
+	exit(8);
+}
+printf("file size has sent to server\n");
+/*============================*/
+
+/*read file content to local buffer*/
+FILE *fp;
+fp=fopen(file_path,"rb");
+if(fp==NULL)
+{
+	perror("fopen FATAL");
+	exit(4);
+}
+
+char *file_content;
+file_content=(char *)malloc(file_size);
+if(file_content==NULL)
+{
+	perror("malloc FATAL");
+	exit(5);
+}
+
+size_t fread_ret;
+fread_ret=fread(file_content,file_size,1,fp);
+if(fread_ret==0)
+{
+	perror("fread FATAL");
+	exit(6);
+}else{
+	printf("fread success\n");
+}
+/*===========================*/
+
+/*send file content to server*/
+write_ret=write(socket_fd,file_content,file_size);
+if(write_ret==-1)
+{
+	perror("write FATAL");
+	exit(7);
+}else{
+	printf("write content to server success\n");
+}
+/*===========================*/
+
+fclose(fp);
 close(socket_fd);
+printf("Goodbye!\n");
 return(0);
 }
